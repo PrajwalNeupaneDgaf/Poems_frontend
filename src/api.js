352d6066c84a29a3d5d0108da02caf3d8ee9c27e
PrajwalNeupaneDgaf <157794663+PrajwalNeupaneDgaf@users.noreply.github.com>
2025-04-11 08:38:45 +0545

    Updated Backend Url

--- src/api.js
+++ src/api.js
@@ -4,6 +4,7 @@ import axios from "axios";
 const api = axios.create({
   //baseURL: "http://localhost:5000/api", 
   baseURL: "https://poems-backend.onrender.com/api", 
+  credentials: 'include', // VERY IMPORTANT
   headers:{
     "Content-Type": "application/json",
   }
